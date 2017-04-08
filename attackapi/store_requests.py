e6ced78f39186ee21f8c82a890623dae049a66fe
chillaranand <anand21nanda@gmail.com>
2017-04-08 12:59:26 +0530

    Ignored unwanted requests

--- attackapi/store_requests.py
+++ attackapi/store_requests.py
@@ -2,6 +2,8 @@ import pickle
 
 import redis
 
+from attackapi.utils import can_ignore, Colors, color_print
+
 
 rc = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -9,6 +11,13 @@ rc = redis.StrictRedis(host='localhost', port=6379, db=0)
 def response(flow):
     client_address = str(flow.client_conn.address).split(':')[0]
     path = flow.request.path
+    r = flow.request
+    if can_ignore(r):
+        url = '{}://{}{}'.format(r.scheme, r.host, r.path)[:100]
+        striked_url = '\u0336'.join(url) + '\u0336'
+        text = 'IGNORED: {}'.format(striked_url)
+        color_print(text, Colors.OKBLUE)
+        return
     req = dict(
         client_address=client_address,
         path=path,
@@ -26,4 +35,5 @@ def response(flow):
     pkl = pickle.dumps(req)
     rc.publish('attackapi', pkl)
     r = flow.request
-    print('Tracked: {}://{}{}'.format(r.scheme, r.host, r.path)[:100])
+    text = 'TRACKED: {}://{}{}'.format(r.scheme, r.host, r.path)[:100]
+    color_print(text, Colors.OKGREEN)
